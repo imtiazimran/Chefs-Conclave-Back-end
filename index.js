9eb19b604084524834853d04479bbe7a1fc6af4f
Imran <imtiazimran@outlook.com>
2023-05-02 02:51:04 +0600

    chef and recipes data added

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 8570
+const cors = require('cors');
+const recipies = require('./recipie.json')
+const chef = require('./Chef_Info.json')
+
+app.use(cors())
 
 app.get('/', (req, res) =>{
     res.send('hello World !')
+})
+app.get('/recipies', (req, res) =>{
+    res.send(recipies)
+})
+
+app.get('/chef', (req, res) =>{
+    res.send(chef)
 })
 
 app.listen(port, ()=>{
